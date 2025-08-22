87d172cc7927d387b0eace4d3d70ce68f992652a
Dynamicbidisha <priyadarsinibidisha97@gmail.com>
2025-08-22 12:58:40 +0530

    native place

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function showSection(sectionId) {
   const sections = document.querySelectorAll('.section');
   sections.forEach(sec => sec.style.display = 'none');
@@ -9,15 +9,3 @@ function showSection(sectionId) {
     target.scrollIntoView({ behavior: 'smooth' });
   }
 }
-=======
-function showSection(sectionId) {
-  const sections = document.querySelectorAll('.section');
-  sections.forEach(sec => sec.style.display = 'none');
-
-  const target = document.getElementById(sectionId);
-  if (target) {
-    target.style.display = 'block';
-    target.scrollIntoView({ behavior: 'smooth' });
-  }
-}
->>>>>>> 3e131e82c06c9f1c912f1e43668af7128c468a74
